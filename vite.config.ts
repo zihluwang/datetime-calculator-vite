ed12af5fcdfbd2bb268807f313b6ebec55c340e9
zihluwang <really@zihlu.wang>
2023-07-24 00:28:29 +0800

    build: add electron to project

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,36 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url"
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+import path from "path"
+
+import electron from "vite-plugin-electron"
+import renderer from "vite-plugin-electron-renderer"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    electron([
+      {
+        // Main-Process entry file of the Electron App.
+        entry: "electron/main.ts"
+      },
+      {
+        entry: "electron/preload.ts",
+        onstart(options) {
+          // Notify the Renderer-Process to reload the page when the Preload-Scripts build is complete,
+          // instead of restarting the entire Electron App.
+          options.reload()
+        }
+      }
+    ]),
+    renderer()
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      "@": fileURLToPath(new URL("./src", import.meta.url))
     }
-  }
+  },
+  // base: path.resolve(__dirname, "./dist/")
 })
